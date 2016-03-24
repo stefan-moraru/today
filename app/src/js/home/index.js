7f878359f98a3c722892bd360324b03fe3cea288
Stefan Moraru <stefan@stefanmoraru.ro>
2016-03-24 22:28:32 +0200

    Start to improve the components on the Homepage

--- app/src/js/home/index.js
+++ app/src/js/home/index.js
@@ -23,8 +23,13 @@ class HomeJumbotron extends Jumbotron {
 
     return (
       <div>
-        <h1 className='f-bold'>{ this.props.title }</h1>
-        <p className='lead'>{ this.props.description }</p>
+        <h1 className='f-bold display-4'>{ this.props.title }</h1>
+        <h3 className='f-light'>{ this.props.description }</h3>
+
+        <div className='arrow'>
+          <p>Afla mai multe</p>
+          <i className='fa fa-chevron-down'></i>
+        </div>
       </div>
     );
 
@@ -36,8 +41,12 @@ class Title extends React.Component {
 
   render() {
 
+    const props = {
+      className: this.props.extraClasses || ''
+    };
+
     return (
-      <div>
+      <div {...props}>
         { this.generateContent() }
       </div>
     );
@@ -47,6 +56,88 @@ class Title extends React.Component {
 }
 
 class HomeTitle extends Title {
+
+  generateContent() {
+
+    const props = {
+      className: 'row ' + this.props.extraClasses || ''
+    };
+
+    return (
+      <div {...props}>
+        <div className='col-xs-12'>
+          <h3>{ this.props.title }</h3>
+        </div>
+      </div>
+    );
+
+  }
+
+}
+
+class Description extends React.Component {
+
+  render() {
+
+    const props = {
+      className: this.props.extraClasses || ''
+    };
+
+    return (
+      <div {...props}>
+        { this.generateContent() }
+      </div>
+    );
+
+  }
+
+}
+
+class HomeDescription extends Description {
+
+  generateContent() {
+
+    const props = {
+      className: 'row ' + this.props.extraClasses || ''
+    };
+
+    return (
+      <div {...props}>
+        <div className='col-xs-12'>
+          <p>{ this.props.description }</p>
+        </div>
+      </div>
+    );
+
+  }
+
+}
+
+class Container extends React.Component {
+
+  render() {
+
+    return (
+      <div className='container'>
+        { this.props.children }
+      </div>
+    );
+
+  }
+
+}
+
+class Section extends React.Component {
+
+  render() {
+
+    return (
+      <div className='section'>
+        { this.props.children }
+      </div>
+    );
+
+  }
 
 }
 
@@ -58,13 +149,9 @@ class Home extends React.Component {
       <div className='home'>
         <HomeJumbotron title='Manageriaza-ti timpul' description='Mai usor ca niciodata' />
 
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xs-12'>
-              <h3>Te-ai saturat sa iti pierzi timpul fara sa iti dai seama ?</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.</p>
-            </div>
-          </div>
+        <Container>
+          <HomeTitle title='Te-ai saturat sa iti pierzi timpul fara sa iti dai seama ?' />
+          <HomeDescription description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.' />
 
           <div className='row c-three-columns'>
             <div className='col-md-4'>
@@ -86,17 +173,14 @@ class Home extends React.Component {
               <img src='http://placehold.it/300x400' />
             </div>
             <div className='col-md-8'>
-              <h2>Responsive</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.</p>
+              <HomeTitle title='Responsive' />
+              <HomeDescription description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.' />
             </div>
           </div>
 
           <div className='c-home-reviews'>
-            <div className='row'>
-              <div className='col-xs-12'>
-                <h3>Vezi ce spune lumea despre noi</h3>
-              </div>
-            </div>
+            <HomeTitle title='Vezi ce spune lumea despre noi' />
+
             <div className='row'>
               <div className='col-xs-12'>
                 <img src='http://placehold.it/100x100' />
@@ -121,11 +205,7 @@ class Home extends React.Component {
           </div>
 
           <div className='c-home-video'>
-            <div className='row'>
-              <div className='col-xs-12 u-hz-ctr'>
-                <h3>Demo</h3>
-              </div>
-            </div>
+            <HomeTitle title='Demo' extraClasses='u-hz-ctr' />
 
             <div className='row'>
               <div className='col-xs-12 u-hz-ctr'>
@@ -136,8 +216,8 @@ class Home extends React.Component {
 
           <div className='row'>
             <div className='col-md-8'>
-              <h2>Responsive</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.</p>
+              <HomeTitle title='Responsive' />
+              <HomeDescription description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.' />
             </div>
             <div className='col-md-4'>
               <img src='http://placehold.it/300x400' />
@@ -149,16 +229,12 @@ class Home extends React.Component {
               <img src='http://placehold.it/300x400' />
             </div>
             <div className='col-md-8'>
-              <h2>Responsive</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.</p>
+              <HomeTitle title='Responsive' />
+              <HomeDescription description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.' />
             </div>
           </div>
 
-          <div className='row'>
-            <div className='col-xs-12 u-hz-ctr'>
-              <h2>Te-am convins ?</h2>
-            </div>
-          </div>
+          <HomeTitle title='Te-am convins ?' extraClasses='u-hz-ctr' />
 
           <div className='row'>
             <div className='col-xs-12 u-hz-ctr'>
@@ -167,11 +243,7 @@ class Home extends React.Component {
           </div>
 
           <div className='c-home-social'>
-            <div className='row'>
-              <div className='col-xs-12'>
-                <h4>Pentru mai multe detalii poti intra pe:</h4>
-              </div>
-            </div>
+            <HomeTitle title='Pentru mai multe detalii poti intra pe:' />
 
             <div className='row'>
               <div className='col-xs-12'>
@@ -185,8 +257,8 @@ class Home extends React.Component {
           <div className='c-home-creators'>
             <div className='row'>
               <div className='col-xs-12'>
-                <h4>Creatori</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.</p>
+                <HomeTitle title='Creatori' />
+                <HomeDescription description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui.' />
               </div>
             </div>
 
@@ -201,7 +273,7 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        </Container>
         <Link to='/logout'>Log out</Link>
         <Link to='/login'>Login</Link>
       </div>
