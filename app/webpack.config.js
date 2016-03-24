b07690381991f5912d35a671b528c91cac75435b
Stefan Moraru <stefan@stefanmoraru.ro>
2016-03-24 19:31:34 +0200

    Initialise app

---
+++ app/webpack.config.js
@@ -0,0 +1,100 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
+/*
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: [
+		'./src/index.jsx'
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: 'dist/'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loaders: ['react-hot', 'babel'],
+			},
+			{
+				test: /\.html$/,
+				loader: 'file?name=[name].[ext]'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			},
+			{
+				test: /\.less$/,
+				loader: 'style-loader!css-loader!less-loader'
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "file"
+			},
+			{
+				test: /\.(woff|woff2)$/,
+				loader: "url?prefix=font/&limit=5000"
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=application/octet-stream"
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: "url?limit=10000&mimetype=image/svg+xml"
+			},
+			{
+				test: /\.gif/,
+				loader: "url-loader?limit=10000&mimetype=image/gif"
+			},
+			{
+				test: /\.jpg/,
+				loader: "url-loader?limit=10000&mimetype=image/jpg"
+			},
+			{
+				test: /\.png/,
+				loader: "file?name=[name].[ext]"
+			}
+		]
+	},
+	devServer: {
+		contentBase: "./dist"
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	]
+};
+*/
