6d1e079673bdaec34b81af4967e146cebc8a7b6d
Stefan Moraru <stefan@stefanmoraru.ro>
2016-03-25 20:19:28 +0200

    Improve Notfound

--- app/src/js/notfound/index.js
+++ app/src/js/notfound/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class NotFound extends React.Component {
 
   render() {
 
-    return <h1>Not found</h1>;
+    return (
+      <div className='col-xs-12 u-hz-ctr u-mt-full'>
+       <h1>Pagina nu a fost gasita</h1>
+       <Link to='/home'>
+         <h4>Intoarce-te pe prima pagina</h4>
+       </Link>
+     </div>
+   );
 
   }
 
