a735c9ef16cb3610b5c021e13df7efe69839d25f
Stefan Moraru <stefan@stefanmoraru.ro>
2016-04-22 00:47:43 +0300

    Get events from Google Calendar

--- app/src/js/common/services/authenticationService/index.js
+++ app/src/js/common/services/authenticationService/index.js
@@ -1,30 +1,78 @@
+/* eslint-disable */
+import moment from 'moment';
 import FbUtils from 'common/utils/firebase';
+
+function loadEvents() {
+
+  return new Promise((resolve, reject) => {
+    var request = gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'showDeleted': false,
+      'timeMin': moment('2016-03-15', 'YYYY-MM-DD').toISOString(),
+      'singleEvents': true,
+      'maxResults': 30,
+      'orderBy': 'startTime'
+    });
+
+    request.execute(function(resp) {
+      let _events = [];
+      var events = resp.items;
+
+      if (events.length > 0) {
+        for (let i = 0; i < events.length; i++) {
+          const event = events[i];
+
+          const start = moment(event.start.dateTime);
+          const end = moment(event.end.dateTime);
+          const duration = (end.hour() * 60 + end.minute()) - (start.hour() * 60 + start.minute());
+
+          const eventConstructed = {
+            id: event.id || '',
+            location: event.location || '',
+            description: event.description || '',
+            title: event.summary || '',
+            creator: {
+              name: event.creator.displayName || '',
+              email: event.creator.email
+            },
+            date: moment(event.start.dateTime, 'YYYY-MM-DD').format('YYYY-MM-DD'),
+            duration: duration || 0
+          };
+
+          _events.push(eventConstructed)
+
+        }
+      }
+
+      resolve(_events);
+    });
+  });
+
+}
 
 const AuthenticationService = {
 
   loginWithProvider(provider) {
 
+    let scopes = {
+      scope: 'email'
+    };
+
+    if (provider === 'google') {
+      scopes = {
+        scope: 'email, https://www.googleapis.com/auth/calendar'
+      }
+    }
+
     FbUtils.ref.authWithOAuthPopup(provider, function(error, authData) {
 
       if (authData) {
-        //Get user with this providers id
-        /*
-          user: {
-            email: '',
-            twitter: ..,
-            google: ..,
-            facebook: ..,
-            twitterData: { },
-            googleData: { },
-            facebookData: { }
-          }
-        */
-
         const providerData = authData[provider];
         const id = providerData.id;
         const email = providerData.email;
         const displayName = providerData.displayName;
         const image = providerData.profileImageURL;
+        const emailFormatted = email.toLowerCase().replace(/\./g, ',');
 
         FbUtils.getUserWithEmail(email)
         .then(found => {
@@ -49,15 +97,28 @@ const AuthenticationService = {
 
           }
 
-          window.location = '/today';
+          //TODO
+          const token = {
+            access_token: authData.google.accessToken //eslint-disable-line
+          };
+
+          gapi.auth.setToken(token); //eslint-disable-line
+          gapi.client.load('calendar', 'v3', () => {
+            loadEvents().then(events => {
+              FbUtils.ref
+              .child('users')
+              .child(emailFormatted)
+              .update({ events: events });
+
+              window.location = '/today';
+            });
+          });
 
         });
 
       }
 
-    }, {
-      scope: 'email'
-    });
+    }, scopes);
 
   },
 
